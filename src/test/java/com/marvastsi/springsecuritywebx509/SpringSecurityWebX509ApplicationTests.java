402efc42e6f22405ec626f04cdff6107694aab29
Marcelo Ferreira Vasconcelos <marcelo.fvas@gmail.com>
2021-05-05 17:01:18 -0300

    initial commit

---
+++ src/test/java/com/marvastsi/springsecuritywebx509/SpringSecurityWebX509ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvastsi.springsecuritywebx509;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWebX509ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
